82bbd7c114d0f8d4aa46d93dd95e9af7c5f2b382
sebastianmct <seba.caamano@duocuc.cl>
2025-10-19 19:26:34 -0300

    cambio

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
-  server: {
-    historyApiFallback: true,
-  },
-  css: {
-    minify: "esbuild",
-  },
+  // base debe ser el nombre del repo precedido y terminado por slash
+  base: '/pizza-time-frontend-only.github.io/',
   build: {
-    cssCodeSplit: true,
+    outDir: 'dist', // carpeta de salida, Vite ya usa 'dist' por defecto
   },
-});
+})
